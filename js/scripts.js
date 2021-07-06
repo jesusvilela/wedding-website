8ca4b695aaa934e65096d2826d5891917131adf1
jesusvilela <jesusvilela@hotmail.es>
2021-07-06 15:17:09 +0200

    Refreshed Google API key and Location Script

--- js/scripts.js
+++ js/scripts.js
@@ -241,7 +241,7 @@ $(document).ready(function () {
 
 // Google map
 function initMap() {
-    var location = {lat: 43.267403, lng: -3.6927923};
+    var location = {lat: 43.268669, lng: -2.934012};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: location,
